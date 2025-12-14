9b5819585f553e3b32cfe56fe780b585a4c6e472
Thang <Thangvm515@gmail.com>
2025-12-15 02:54:32 +0700

    Thực hành tạo Task-list-app

---
+++ task-list-app/database/seeders/TaskSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Task;
+use Faker\Factory as Faker;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+    foreach (range(1,10) as $i) {
+        Task::create([
+            'title' => $faker->sentence(),
+            'description' => $faker->paragraph(),
+            'long_description' => $faker->optional()->text(),
+            'completed' => $faker->boolean(),
+        ]);
+    }
+    }
+}
