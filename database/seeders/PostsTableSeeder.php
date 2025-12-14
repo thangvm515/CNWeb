c1a930a032e3ca70e7323329879071fc4d9ebb54
Thang <Thangvm515@gmail.com>
2025-12-15 01:31:04 +0700

    Tạo lr-project

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -2,23 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
 class PostsTableSeeder extends Seeder
 {
-    /**
-    * Run the database seeds.
-    */
     public function run(): void
     {
         $faker = Faker::create();
-        
-        for ($i = 0; $i < 100; $i++) {
+
+        for ($i = 0; $i < 20; $i++) {
             DB::table('posts')->insert([
                 'title' => $faker->sentence,
-                'content' => $faker->paragraph
+                'content' => $faker->paragraph,
+                'created_at' => now(),
+                'updated_at' => now(),
             ]);
         }
     }
